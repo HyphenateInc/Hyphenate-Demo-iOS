fe7b1e47e227da8b0b5cfed797a9140b1a6435b7
Neil <cao_lihui@msn.com>
2016-08-24 15:36:48 +0800

    fix typo

--- ChatDemo-UI3.0/Class/Call/CallViewController.m
+++ ChatDemo-UI3.0/Class/Call/CallViewController.m
@@ -439,7 +439,11 @@
 - (void)silenceAction
 {
     self.silenceButton.selected = !self.silenceButton.selected;
-    [[EMClient sharedClient].callManager pauseVoiceWithSession:_callSession.sessionId error:nil];
+    if (_silenceButton.selected) {
+        [[EMClient sharedClient].callManager pauseVoiceWithSession:_callSession.sessionId error:nil];
+    } else {
+        [[EMClient sharedClient].callManager resumeVoiceWithSession:_callSession.sessionId error:nil];
+    }
 }
 
 - (void)speakerOutAction
